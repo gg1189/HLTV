5051455d97ba7013b405d39a84d2c6763288d799
gg1189 <scg2007116@hotmail.com>
2026-03-19 16:14:32 +0800

    Update getNewsContent.ts

--- src/endpoints/getNewsContent.ts
+++ src/endpoints/getNewsContent.ts
@@ -1,94 +1,175 @@
-import { HLTVConfig } from '../config'
-import { HLTVScraper } from '../scraper'
-import { fetchPage, generateRandomSuffix } from '../utils'
+// src/index.ts
+import { defaultConfig, defaultLoadPage, HLTVConfig } from './config'
+import { getMatch } from './endpoints/getMatch'
+import { connectToScorebot } from './endpoints/connectToScorebot'
+import { getMatches } from './endpoints/getMatches'
+import { getEvent } from './endpoints/getEvent'
+import { getEventByName } from './endpoints/getEventByName'
+import { getEvents } from './endpoints/getEvents'
+import { getMatchMapStats } from './endpoints/getMatchMapStats'
+import { getMatchStats } from './endpoints/getMatchStats'
+import { getMatchesStats } from './endpoints/getMatchesStats'
+import { getPlayer } from './endpoints/getPlayer'
+import { getPlayerByName } from './endpoints/getPlayerByName'
+import { getPlayerRanking } from './endpoints/getPlayerRanking'
+import { getPlayerStats } from './endpoints/getPlayerStats'
+import { getRecentThreads } from './endpoints/getRecentThreads'
+import { getStreams } from './endpoints/getStreams'
+import { getTeam } from './endpoints/getTeam'
+import { getTeamByName } from './endpoints/getTeamByName'
+import { getTeamRanking } from './endpoints/getTeamRanking'
+import { getTeamStats } from './endpoints/getTeamStats'
+import { getPastEvents } from './endpoints/getPastEvents'
+import { getResults } from './endpoints/getResults'
+import { getNews } from './endpoints/getNews'
+import { getRssNews } from './endpoints/getRssNews'
+import { getNewsContent } from './endpoints/getNewsContent'  // ← 新增這行匯出 getNewsContent
 
-export interface NewsBlock {
-  type: 'paragraph' | 'image' | 'header'
-  data: {
-    text?: string
-    url?: string
-    level?: number  // 如果是 header
-  }
-}
-
-export interface NewsContent {
-  id: string | number
-  date: string                    // ISO 格式，例如 "2026-03-19T08:38:00.000Z"
-  title: string
-  author: string
-  body: {
-    blocks: NewsBlock[]
-  }
-  image_url?: string
-  event?: {
-    name: string
-    id?: number
-  }
-}
-
-export const getNewsContent =
-  (config: HLTVConfig) => async ({ id }: { id: number | string }): Promise<NewsContent> => {
-    const url = `https://www.hltv.org/news/${id}/${generateRandomSuffix()}`
-    const $ = HLTVScraper(await fetchPage(url, config.loadPage))
-
-    const title = $('h1.headline').text().trim() || '無標題'
-
-    const author = $('.author-date-con .author a').text().trim() || '未知作者'
-
-    const dateText = $('.date').attr('data-unix')
-    const date = dateText ? new Date(Number(dateText)).toISOString() : ''
-
-    const image_url = $('.image-con img').attr('src') || $('.image-con picture source').attr('srcset')?.split(' ')[0] || undefined
-
-    const eventName = $('.event a').text().trim()
-    const eventHref = $('.event a').attr('href')
-    const eventId = eventHref ? Number(eventHref.match(/\/events\/(\d+)/)?.[1]) : undefined
-
-    // 抓取內容並轉成 blocks 結構
-    const blocks: NewsBlock[] = []
-
-    // 遍歷 .newstext-con 裡的所有子元素
-    $('.newsdsl .newstext-con').children().each((_, el) => {
-      const $el = $(el)
-      const tag = $el.prop('tagName').toLowerCase()
-
-      if (tag === 'p') {
-        const text = $el.text().trim()
-        if (text) {
-          blocks.push({
-            type: 'paragraph',
-            data: { text }
-          })
-        }
-      } else if (tag === 'img' || tag === 'picture' || tag === 'figure') {
-        const imgSrc = $el.find('img').attr('src') || $el.attr('src') || $el.find('source').attr('srcset')?.split(' ')[0]
-        if (imgSrc) {
-          blocks.push({
-            type: 'image',
-            data: { url: imgSrc }
-          })
-        }
-      } else if (tag.startsWith('h')) {
-        const level = Number(tag.replace('h', ''))
-        const text = $el.text().trim()
-        if (text) {
-          blocks.push({
-            type: 'header',
-            data: { text, level }
-          })
-        }
-      }
-    })
-
-    return {
-      id,
-      date,
-      title,
-      author,
-      body: {
-        blocks
-      },
-      image_url,
-      event: eventName ? { name: eventName, id: eventId } : undefined
+export class Hltv {
+  constructor(private config: Partial<HLTVConfig> = {}) {
+    if (config.httpAgent && !config.loadPage) {
+      config.loadPage = defaultLoadPage(config.httpAgent)
+    }
+    if (!config.httpAgent) {
+      config.httpAgent = defaultConfig.httpAgent
+    }
+    if (!config.loadPage) {
+      config.loadPage = defaultConfig.loadPage
     }
   }
+
+  getMatch = getMatch(this.config as HLTVConfig)
+  getMatches = getMatches(this.config as HLTVConfig)
+  getEvent = getEvent(this.config as HLTVConfig)
+  getEvents = getEvents(this.config as HLTVConfig)
+  getPastEvents = getPastEvents(this.config as HLTVConfig)
+  getEventByName = getEventByName(this.config as HLTVConfig)
+  getMatchMapStats = getMatchMapStats(this.config as HLTVConfig)
+  getMatchStats = getMatchStats(this.config as HLTVConfig)
+  getMatchesStats = getMatchesStats(this.config as HLTVConfig)
+  getPlayer = getPlayer(this.config as HLTVConfig)
+  getPlayerByName = getPlayerByName(this.config as HLTVConfig)
+  getPlayerRanking = getPlayerRanking(this.config as HLTVConfig)
+  getPlayerStats = getPlayerStats(this.config as HLTVConfig)
+  getRecentThreads = getRecentThreads(this.config as HLTVConfig)
+  getStreams = getStreams(this.config as HLTVConfig)
+  getTeam = getTeam(this.config as HLTVConfig)
+  getTeamByName = getTeamByName(this.config as HLTVConfig)
+  getTeamRanking = getTeamRanking(this.config as HLTVConfig)
+  getTeamStats = getTeamStats(this.config as HLTVConfig)
+  getResults = getResults(this.config as HLTVConfig)
+  getNews = getNews(this.config as HLTVConfig)
+  getRssNews = getRssNews(this.config as HLTVConfig)
+  getNewsContent = getNewsContent(this.config as HLTVConfig)  // ← 新增這行，讓 HLTV.getNewsContent 可用
+  connectToScorebot = connectToScorebot(this.config as HLTVConfig)
+
+  public createInstance(config: Partial<HLTVConfig>) {
+    return new Hltv(config)
+  }
+
+  public TEAM_PLACEHOLDER_IMAGE =
+    'https://www.hltv.org/img/static/team/placeholder.svg'
+  public PLAYER_PLACEHOLDER_IMAGE =
+    'https://static.hltv.org/images/playerprofile/bodyshot/unknown.png'
+}
+
+const hltv = new Hltv()
+export default hltv
+export { hltv as HLTV }
+
+// 匯出型別（保持原有，並新增 getNewsContent 相關型別）
+export { MatchStatus } from './endpoints/getMatch'
+export type {
+  Highlight,
+  Veto,
+  HeadToHeadResult,
+  ProviderOdds,
+  MapHalfResult,
+  MapResult,
+  Stream,
+  FullMatch as Match
+} from './endpoints/getMatch'
+
+export { MatchEventType, MatchFilter } from './endpoints/getMatches'
+export type { MatchPreview, GetMatchesArguments } from './endpoints/getMatches'
+
+export { WinType } from './endpoints/connectToScorebot'
+export type { ScoreboardUpdate, LogUpdate } from './endpoints/connectToScorebot'
+
+export type {
+  FullEvent,
+  FullEventHighlight,
+  FullEventFormat,
+  FullEventPrizeDistribution,
+  FullEventTeam
+} from './endpoints/getEvent'
+
+export type { EventPreview, GetEventsArguments } from './endpoints/getEvents'
+
+export type { FullMatchStats } from './endpoints/getMatchStats'
+
+export type {
+  GetMatchesStatsArguments,
+  MatchStatsPreview
+} from './endpoints/getMatchesStats'
+
+export type {
+  FullPlayerTeam,
+  PlayerAchievement,
+  FullPlayer
+} from './endpoints/getPlayer'
+
+export type {
+  PlayerRanking,
+  GetPlayerRankingOptions
+} from './endpoints/getPlayerRanking'
+
+export type {
+  FullPlayerStats,
+  GetPlayerStatsArguments
+} from './endpoints/getPlayerStats'
+
+export { ThreadCategory } from './endpoints/getRecentThreads'
+export type { Thread } from './endpoints/getRecentThreads'
+
+export { StreamCategory } from './endpoints/getStreams'
+export type { FullStream } from './endpoints/getStreams'
+
+export { TeamPlayerType } from './endpoints/getTeam'
+export type { FullTeam, FullTeamPlayer } from './endpoints/getTeam'
+
+export type { TeamRanking, GetTeamArguments } from './endpoints/getTeamRanking'
+
+export type { GetPastEventsArguments } from './endpoints/getPastEvents'
+
+export {
+  ResultsMatchType,
+  ContentFilter,
+  GameType
+} from './endpoints/getResults'
+
+export type {
+  FullMatchResult,
+  ResultTeam,
+  GetResultsArguments
+} from './endpoints/getResults'
+
+export type { NewsPreview, GetNewsArguments } from './endpoints/getNews'
+
+export type { RssArticle } from './endpoints/getRssNews'
+
+// 新增匯出 getNewsContent 的型別
+export type { NewsContent } from './endpoints/getNewsContent'
+
+export { GameMap } from './shared/GameMap'
+export { MatchFormat } from './shared/MatchFormat'
+export { RankingFilter } from './shared/RankingFilter'
+export { MatchType } from './shared/MatchType'
+export { BestOfFilter } from './shared/BestOfFilter'
+
+export type { Article } from './shared/Article'
+export type { Country } from './shared/Country'
+export type { Event } from './shared/Event'
+export type { Player } from './shared/Player'
+export type { Team } from './shared/Team'
+export type { EventType } from './shared/EventType'
