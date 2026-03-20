e5d156cad970804479875a990d177b874b5520a6
gg1189 <scg2007116@hotmail.com>
2026-03-20 09:37:53 +0800

    Update getNewsContent.ts

--- src/endpoints/getNewsContent.ts
+++ src/endpoints/getNewsContent.ts
@@ -1,6 +1,6 @@
 import { HLTVConfig } from '../config'
 import { HLTVScraper } from '../scraper'
-import { fetchPasge, generateRandomSuffix } from '../utils's
+import { fetchPasge, generateRandomSuffix } from '../utils'
 
 export interface NewsContent {
   id: string | number
