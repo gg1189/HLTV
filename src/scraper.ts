6014936225d2ea67f83a321777a9cb44be9cb583
gg1189 <scg2007116@hotmail.com>
2026-03-20 16:26:20 +0800

    Update scraper.ts

--- src/scraper.ts
+++ src/scraper.ts
@@ -35,7 +35,7 @@ export interface HLTVPageElement {
   filter(
     func: (index: number, element: HLTVPageElement) => boolean
   ): HLTVPageElement
-  hasClass(className: string): boolean
+  hasClass(className: string): HLTVPageElement
 }
 
 const attachMethods = (root: cheerio.Cheerio): HLTVPageElement => {
@@ -145,8 +145,8 @@ const attachMethods = (root: cheerio.Cheerio): HLTVPageElement => {
       )
     },
 
-    hasClass(className: string): boolean {
-          return root.hasClass(className)
+    hasClass(className: string): HLTVPageElement {
+          return attachMethods(root.hasClass(className))
     }
 
     index(): number {
